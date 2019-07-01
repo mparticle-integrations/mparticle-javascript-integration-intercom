6d62bce14891e4bb454999d8775043382e9cfda3
Robert Ing <ring@mparticle.com>
2019-07-01 18:08:28 -0400

    Update rollup to build IIFE and CJS, refactor register

--- dist/IntercomEventForwarder.common.js
+++ dist/IntercomEventForwarder.common.js
@@ -1,3 +1,26 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+
+/*!
+ * isobject <https://github.com/jonschlinkert/isobject>
+ *
+ * Copyright (c) 2014-2017, Jon Schlinkert.
+ * Released under the MIT License.
+ */
+
+function isObject(val) {
+  return val != null && typeof val === 'object' && Array.isArray(val) === false;
+}
+
+var isobject = /*#__PURE__*/Object.freeze({
+  'default': isObject
+});
+
+function getCjsExportFromNamespace (n) {
+	return n && n['default'] || n;
+}
+
+var isobject$1 = getCjsExportFromNamespace(isobject);
+
 //
 //  Copyright 2015 mParticle, Inc.
 //
@@ -13,7 +36,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-    var isobject = require('isobject');
+    
 
     var name = 'IntercomEventForwarder',
         moduleId = 18,
@@ -198,12 +221,12 @@
             return;
         }
 
-        if (!isobject(config)) {
+        if (!isobject$1(config)) {
             window.console.log('\'config\' must be an object. You passed in a ' + typeof config);
             return;
         }
 
-        if (isobject(config.kits)) {
+        if (isobject$1(config.kits)) {
             config.kits[name] = {
                 constructor: constructor
             };
@@ -224,6 +247,10 @@
         });
     }
 
-    module.exports = {
+    var IntercomEventForwarder = {
         register: register
     };
+var IntercomEventForwarder_1 = IntercomEventForwarder.register;
+
+exports.default = IntercomEventForwarder;
+exports.register = IntercomEventForwarder_1;
--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,53 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
-export default [{
-    input: 'src/IntercomEventForwarder.js',
-    output: {
-        file: 'IntercomEventForwarder.js',
-        format: 'umd',
-        exports: 'named',
-        name: 'mp-intercom-kit',
-        strict: false
+export default [
+    {
+        input: 'src/IntercomEventForwarder.js',
+        output: {
+            file: 'IntercomEventForwarder.js',
+            format: 'iife',
+            exports: 'named',
+            name: 'mpIntercomKit',
+            strict: false
+        },
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            commonjs()
+        ]
     },
-    plugins: [
-        resolve({
-            browser: true
-        }),
-        commonjs()
-    ]
-},
-{
-    input: 'src/IntercomEventForwarder.js',
-    output: {
-        file: 'dist/IntercomEventForwarder.js',
-        format: 'umd',
-        exports: 'named',
-        name: 'mp-intercom-kit',
-        strict: false
+    {
+        input: 'src/IntercomEventForwarder.js',
+        output: {
+            file: 'dist/IntercomEventForwarder.iife.js',
+            format: 'iife',
+            exports: 'named',
+            name: 'mpIntercomKit',
+            strict: false
+        },
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            commonjs()
+        ]
     },
-    plugins: [
-        resolve({
-            browser: true
-        }),
-        commonjs()
-    ]
-}
-] 
+    {
+        input: 'src/IntercomEventForwarder.js',
+        output: {
+            file: 'dist/IntercomEventForwarder.common.js',
+            format: 'cjs',
+            exports: 'named',
+            name: 'mpIntercomKit',
+            strict: false
+        },
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            commonjs()
+        ]
+    }
+]
